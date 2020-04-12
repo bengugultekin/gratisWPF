2a28c1bca86cd7c17ae01900e97ca0db06a3e3db
Bengünur Gültekin <bengunurgultekin54@gmail.com>
2020-04-12 15:04:12 +0300

    added new buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace gratisWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
